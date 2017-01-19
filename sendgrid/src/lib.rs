ef8eb64266628aea4e993d2de4ef8db42cb3109c
anykao <600913@gmail.com>
2017-01-20 08:17:12 +0900

    update

--- sendgrid/src/lib.rs
+++ sendgrid/src/lib.rs
@@ -6,7 +6,8 @@ extern crate reqwest;
 
 use std::collections::HashMap;
 use reqwest::header::{Authorization, Bearer};
-use reqwest::Client;
+use reqwest::{Client, Response, StatusCode};
+use std::borrow::Cow;
 
 const V3_API_URL: &'static str = "https://api.sendgrid.com/v3/mail/send";
 
@@ -17,8 +18,8 @@ pub struct SGMailV3 {
     from_email: Email,
     subject: String,
     personalizations: Vec<Personalization>,
-    constent: Vec<Content>,
-    attachments: Vec<Attachment>
+    contents: Vec<Content>,
+    attachments: Vec<Attachment>,
     template_id: String,
     sections: Option<SGMap>,
     headers: Option<SGMap>,
@@ -48,12 +49,33 @@ pub struct Email {
     email: String,
 }
 
+impl Email {
+    pub fn new<S>(name: S, email: S) -> Self
+        where S: Into<String>
+    {
+        Email {
+            name: Some(name.into()),
+            email: email.into(),
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Default, Debug)]
 pub struct Content {
     typ: String,
     value: String,
 }
 
+impl Content {
+    pub fn new<S: Into<String>>(typ: S, value: S) -> Self {
+        Content {
+            typ: typ.into(),
+            value: value.into(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
 struct Attachment {
     content: String,
     typ: String,
@@ -63,11 +85,13 @@ struct Attachment {
     content_id: String,
 }
 
+#[derive(Serialize, Deserialize, Default, Debug)]
 struct Asm {
     group_id: i64,
     groups_to_display: Vec<i64>,
 }
 
+#[derive(Serialize, Deserialize, Default, Debug)]
 struct MailSettings {
     bcc: BccSetting,
     bypass_list_management: Setting,
@@ -76,6 +100,7 @@ struct MailSettings {
     spam_checkSetting: SpamCheckSetting,
 }
 
+#[derive(Serialize, Deserialize, Default, Debug)]
 struct TrackingSettings {
     click_tracking: ClickTrackingSetting,
     open_tracking: OpenTrackingSetting,
@@ -87,39 +112,46 @@ struct TrackingSettings {
     sandbox_mode: SandboxModeSetting,
 }
 
+#[derive(Serialize, Deserialize, Default, Debug)]
 struct BccSetting {
     enable: bool,
     email: String,
 }
 
+#[derive(Serialize, Deserialize, Default, Debug)]
 struct FooterSetting {
     enable: bool,
     text: String,
     html: String,
 }
 
+#[derive(Serialize, Deserialize, Default, Debug)]
 struct ClickTrackingSetting {
     enable: bool,
     enable_text: bool,
 }
 
+#[derive(Serialize, Deserialize, Default, Debug)]
 struct OpenTrackingSetting {
     enable: bool,
     substitution_tag: String,
 }
 
+#[derive(Serialize, Deserialize, Default, Debug)]
 struct SandboxModeSetting {
     enable: bool,
     forward_spam: bool,
     spam_check: SpamCheckSetting,
 }
 
+#[derive(Serialize, Deserialize, Default, Debug)]
 struct SpamCheckSetting {
     enable: bool,
     spam_threshold: i64,
     post_to_url: String,
 }
 
+#[derive(Serialize, Deserialize, Default, Debug)]
 struct SubscriptionTrackingSetting {
     enable: bool,
     text: String,
@@ -127,6 +159,7 @@ struct SubscriptionTrackingSetting {
     substitution_tag: String,
 }
 
+#[derive(Serialize, Deserialize, Default, Debug)]
 struct GaSetting {
     enable: bool,
     campaign_source: String,
@@ -136,6 +169,7 @@ struct GaSetting {
     campaign_medium: String,
 }
 
+#[derive(Serialize, Deserialize, Default, Debug)]
 struct Setting {
     enable: bool,
 }
@@ -148,11 +182,13 @@ impl EmailBuilder {
         EmailBuilder(inner)
     }
 
-    pub fn add_from(&mut self, name: Option<String>, address: String) -> &mut Self {
-        self.0.Sender = Email {
-            Name: name,
-            Address: address,
-        };
+    pub fn add_from(&mut self, email: Email) -> &mut Self {
+        self.0.from_email = email;
+        self
+    }
+
+    pub fn add_content(&mut self, content: Content) -> &mut Self {
+        self.0.contents.push(content);
         self
     }
 
@@ -161,17 +197,8 @@ impl EmailBuilder {
                                address: String,
                                subject: String)
                                -> &mut Self {
-        self.0.Subject = subject;
+        self.0.subject = subject;
         self
-    }
-
-    pub fn add_content(&mut self, typ: Option<String>, value: String) -> &mut Self {
-        self.0.Subject = subject;
-        self
-    }
-
-    pub fn finish(&self) -> &SGMailV3 {
-        &self.0
     }
 }
 
@@ -179,6 +206,7 @@ pub struct MailClient {
     client: Client,
     key: String,
 }
+
 impl MailClient {
     pub fn new<T>(api_key: T) -> Self
         where T: Into<String>
@@ -192,7 +220,7 @@ impl MailClient {
 
     }
 
-    pub fn send_mail(&self, mail: SGMailV3) -> Result<(), &str> {
+    pub fn send_mail(self, mail: SGMailV3) -> Response {
         self.client
             .post(V3_API_URL)
             .header(Authorization(Bearer { token: self.key }))
@@ -202,9 +230,11 @@ impl MailClient {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {}
+    fn it_works() {
+        EmailBuilder::new().add_from(Email::new("1", "2")).add_content(Content::new("3", "4"));
+    }
 }
