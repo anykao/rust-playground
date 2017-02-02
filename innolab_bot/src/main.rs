76a8a094a9ff5f96dd87c21d467ea9dd2813924a
meng.fanjun <xmeng.fanjun@contract.isid.co.jp>
2017-02-02 11:11:07 +0900

    add telebot

---
+++ innolab_bot/src/main.rs
@@ -0,0 +1,49 @@
+extern crate tg_botapi;
+extern crate dotenv;
+
+use dotenv::dotenv;
+use tg_botapi::args;
+use tg_botapi::BotApi;
+
+use std::sync::Arc;
+use std::thread;
+use std::env;
+
+fn main() {
+    dotenv().ok();
+    // Create bot, test simple API call and print bot information
+    let token = env::var("TG_TOKEN").unwrap();
+    println!("{}", token);
+
+    let bot_arc = Arc::new(BotApi::new(&token));
+
+    let mut update_args = args::GetUpdates::new().timeout(600).offset(0);
+
+    'update_loop: loop {
+        let updates = bot_arc.get_updates(&update_args).unwrap();
+
+        for update in updates {
+            update_args.offset = Some(update.update_id + 1);
+
+            if let Some(message) = update.message {
+                println!("{:?}", message);
+                let bot = bot_arc.clone();
+
+                thread::spawn(move || {
+                    let chat_id = message.chat.id;
+                    let msg_id = message.message_id;
+
+                    let message_text = format!("\"{}\"\n    - <i>You, CURRENT_YEAR</i>",
+                                               message.text.unwrap_or(String::new()));
+
+                    let _ = bot.send_message(&args::SendMessage::new(&message_text)
+                        .chat_id(chat_id)
+                        .parse_mode("HTML"));
+                });
+            }
+        }
+    }
+    update_args.limit = Some(0);
+    update_args.timeout = Some(0);
+    let _ = bot_arc.get_updates(&update_args);
+}
